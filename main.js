35ad69cff51a008972f1c525030a0d8e04943b10
Deniss Bukovskis <A230224DB@rvt.lv>
2025-10-14 11:24:16 +0300

    pievienoju login modal1

--- main.js
+++ main.js
@@ -77,3 +77,43 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+document.getElementById('loginTab').addEventListener('click', function () {
+  document.getElementById('loginForm').classList.add('active');
+  document.getElementById('registerForm').classList.remove('active');
+  this.classList.add('active');
+  document.getElementById('registerTab').classList.remove('active');
+});
+
+document.getElementById('registerTab').addEventListener('click', function () {
+  document.getElementById('registerForm').classList.add('active');
+  document.getElementById('loginForm').classList.remove('active');
+  this.classList.add('active');
+  document.getElementById('loginTab').classList.remove('active');
+});
